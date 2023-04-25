a42c2468b4d85d82fcd54f7614937f3cc3bb285b
Angela <98893476+amyers0217@users.noreply.github.com>
2023-04-25 11:31:32 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/** NAVBAR TOGGLE */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navClseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElems = [overlay, navOpenBtn, navClseBtn];
+
+for (let i = 0; i < navElems.length; i++) {
+  navElems[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
